6d7017e12b7f9efc70a5781980867945b9a67964
Hannah Malm <hannahemalm616@gmail.com>
2022-02-08 10:33:22 -0600

    setting up initial redux store

--- src/index.js
+++ src/index.js
@@ -3,11 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createStore, applyMiddleware, compose, combineReducers} from 'redux'
+import thunk from 'redux-thunk'
+import {Provider} from 'react-redux'
+//Three libraries to import from: redux, redux-thunk, react-redux
 
+const users = () => []
+const reducer = combineReducers({
+  users
+})
+
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducer, composeEnhancer(applyMiddleware(thunk)))
+
+
+//Standard boilerplate code - Wrap provider in the store
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}><App />
+  </Provider>,
   document.getElementById('root')
 );
 
