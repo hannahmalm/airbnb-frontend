01577e41395369d5b4cecd0cbac3c3c931ab370a
Hannah Malm <hannahemalm616@gmail.com>
2022-02-08 17:23:07 -0600

    added in login form

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,2 +1,16 @@
 //Components must have capital letter 
 //Since this is built in redux, need a reducer and action
+import React from 'react'
+import { login } from '../actions/currentUser'
+
+const Login = () => {
+    return (
+        <form onSubmit={}>
+            <input type="text" onChange={} value={} name="username"></input>
+            <input type="text" onChange={} value={} name="password"></input>
+            <input type="submit" value="Log In"></input>
+        </form>
+    )
+}
+
+export default login;
