1b20b267d0bc64914dabbdda0562c80828899500
Hannah Malm <hannahemalm616@gmail.com>
2022-02-08 17:57:50 -0600

    adding login form component

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,16 +1,28 @@
 //Components must have capital letter 
+//STATEFUL component 1
 //Since this is built in redux, need a reducer and action
 import React from 'react'
-import { login } from '../actions/currentUser'
+//always need to import action creator
+import { connect } from 'react-redux'
 
-const Login = () => {
+//props get passed into a functional component as an argument & object
+const Login = ({username, password}) => {
     return (
         <form onSubmit={}>
-            <input type="text" onChange={} value={} name="username"></input>
-            <input type="text" onChange={} value={} name="password"></input>
+            <input type="text" onChange={} value={username} name="username" placeholder='username'></input>
+            <input type="text" onChange={} value={password} name="password" placeholder='password'></input>
             <input type="submit" value="Log In"></input>
         </form>
     )
 }
 
-export default login;
+//gives argument coming to component
+const mapStateToProps = state => {
+    //this is what connects the data coming in here to the store
+    //what state is needed? Username and Pasword
+    //Get the state from the store and lets us use these as props
+    username: state.loginForm.username
+    pasword: state.loginForm.password
+}
+
+export default connect(mapStateToProps)(login);
