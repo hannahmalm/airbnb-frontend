6959dd32a5fb217edbc3c8b3670c853226543b94
Hannah Malm <hannahemalm616@gmail.com>
2022-02-08 17:24:09 -0600

    added login form to app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import './App.css';
+import LoginForm from "./components/LoginForm.js"
 
 class App extends React.Component {
   render(){
     return (
-      "testing"
+      <Login/>
     );
     }
   }
