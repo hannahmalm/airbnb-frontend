6bc0a137538667b0e20572db0b1351d915ad53ab
Hannah Malm <hannahemalm616@gmail.com>
2022-02-08 16:58:33 -0600

    adding login functionality in the action

--- src/actions/currentUser.js
+++ src/actions/currentUser.js
@@ -1,8 +1,19 @@
+//synchronous action creator
 export const setCurrentUser = user => {
     return {
     //return an action -> key of type with a payload
     //an action is an object
     type: "SET_CURRENT_USER", //THIS IS THE SAME TYPE AS THE ONE IN THE REDUCER
     payload: user
+    }
+}
+
+//In order to become a current user, you must log in 
+//asynchronous request - need to send a request to backend 
+//return a function --> Thunk gives us this capability
+//auth controller --> Ensure this is set up on the backend 
+export const login = credentials => {
+    return dispatch => {
+        return fetch("http://localhost:3000/")
     }
 }
