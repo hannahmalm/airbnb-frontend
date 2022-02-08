5b968f1568792019b636943dc23b95325a95f160
Hannah Malm <hannahemalm616@gmail.com>
2022-02-08 17:19:48 -0600

    added in login form reducer and action

---
+++ src/actions/loginForm.js
@@ -0,0 +1,10 @@
+import loginForm from "../reducers/loginForm"
+
+export const updateLoginForm = loginFormData => {
+    return {
+    //return an action -> key of type with a payload
+    //an action is an object
+    type: "UPDATE_LOGIN_FORM", //THIS IS THE SAME TYPE AS THE ONE IN THE REDUCER
+    payload: loginFormData
+    }
+}
