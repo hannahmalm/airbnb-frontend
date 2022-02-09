703a10df3ba8dd609b58cbb155b77ddefd83c5dc
Hannah Malm <hannahemalm616@gmail.com>
2022-02-09 11:31:45 -0600

    troubleshooting props and state

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -5,8 +5,15 @@ import { updateLoginForm } from '../actions/login.js' //import login form
 
 //basic login form boilerplate
 //props get passed into functional component as an argument & object
-const Login = (props, updateLoginForm) => {
-
+//const Login = (props, updateLoginForm) => {
+const Login = ({username, password, updateLoginForm}) => {
+     return (
+        <form onSubmit={undefined}>
+            <input name="username" type="text" value={username} placeholder='Username' onChange={undefined}/>
+            <input name="password" type="text" value={password} placeholder='Password' onChange={undefined}/>
+            <input type="submit" value="Log In"></input>
+        </form>
+    )
 
     //onSubmit is when the form submit button is pressed
     // const handleSubmit = e => {
@@ -15,32 +22,32 @@ const Login = (props, updateLoginForm) => {
     //     // login(loginFormData)
     // }
 
-    return (
-        <form id="login_form" onSubmit={undefined}>
-            <input name="username" type="text" value={props.username} placeholder='Username' onChange={(e) => updateLoginForm({undefined})}/>
-            {/* <input name="password" type="text" value={props.password} placeholder='Password' onChange={(e) => updateLoginForm({name: e.target.name, value: e.target.value})}/> */}
-            <input type="submit" value="Log In"></input>
-        </form>
-
-    )
-
+    // return (
+    //     <form onSubmit={undefined}>
+    //         <input name="username" type="text" value={props.username} placeholder='Username' onChange={(e) => updateLoginForm({undefined})}/>
+    //         <input name="password" type="text" value={props.password} placeholder='Password' onChange={(e) => updateLoginForm({name: e.target.name, value: e.target.value})}/>
+    //         <input type="submit" value="Log In"></input>
+    //     </form>
+    // )
 }
 
 
 // Connect - Extracting data with mapStateToProps
 // mapStateToProps is used for selecting the part of the data from the store that the connected component needs. 
 //mapState is what connects the data to the store -> take sttate from redux to get what the state is from redux
-// function mapStateToProps(state) {
-//     //Gives access to these parts of state as props --> Get the username and password from the store
-//   return { 
-//       username: state.loginForm.username,
-//       password: state.loginForm.password
-//    }
-//  }
+const mapStateToProps = state => {
+    //Gives access to these parts of state as props --> Get the username and password from the store
+    //If you dont know this, look at the store in redux
+  return { 
+      username: state.login.username,
+      password: state.login.password
+   }
+ }
   
  //ability to connect from react-redux
  //returns a component(the Login component)
  //mapstae is the first argument in connect
  //second argument is 
-//   export default connect(mapStateToProps, {updateLoginForm: updateLoginForm})(Login);
-export default Login
+ // export default connect(mapStateToProps, {updateLoginForm: updateLoginForm})(Login)
+// export default Login
+export default connect(mapStateToProps)(Login)
