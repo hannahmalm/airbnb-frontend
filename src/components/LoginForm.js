8b6624aa72f64e578270b505ede4c7c05f466ab1
Hannah Malm <hannahemalm616@gmail.com>
2022-02-09 10:16:39 -0600

    adding mapstate to prop in login form

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -17,8 +17,8 @@ const Login = (loginFormData) => {
 
     return (
         <form id="login_form" onSubmit={handleSubmit}>
-            <input name="username" type="text" value={loginFormData.username} placeholder='Username'/>
-            <input name="password" type="text" value={loginFormData.password} placeholder='Password'/>
+            <input name="username" type="text" value={loginFormData.username} placeholder='Username' onChange={}/>
+            <input name="password" type="text" value={loginFormData.password} placeholder='Password' onChange={}/>
             <input type="submit" value="Log In"></input>
         </form>
 
@@ -29,11 +29,17 @@ const Login = (loginFormData) => {
 
 // Connect - Extracting data with mapStateToProps
 // mapStateToProps is used for selecting the part of the data from the store that the connected component needs. 
+//mapState is what connects the data to the store -> take sttate from redux to get what the state is from redux
 function mapState(state) {
     // const { login } = state
   return { 
-      loginFormData: state.loginForm
+      username: state.loginForm.username,
+      password: state.loginForm.password
    }
  }
   
-  export default connect(mapState, {login})(Login);
+ //ability to connect from react-redux
+ //returns a component(the Login component)
+ //mapstae is the first argument in connect
+ //second argument is 
+  export default connect(mapState)(Login);
