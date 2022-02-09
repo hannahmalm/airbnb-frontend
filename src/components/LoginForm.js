14f0db21500b0b16610f4537e9028c327d7fb2a8
Hannah Malm <hannahemalm616@gmail.com>
2022-02-09 10:36:25 -0600

    updating onChange for testing

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import {connect} from 'react-redux' //The connect() function connects a React component to a Redux store.
-import { login } from "../actions/currentUser.js"
+import { updateLoginForm } from '../actions/login.js' //import login form 
 
 
 //basic login form boilerplate
 //props get passed into functional component as an argument & object
-const Login = (props) => {
+const Login = (props, updateLoginForm) => {
 
 
     //onSubmit is when the form submit button is pressed
@@ -17,8 +17,8 @@ const Login = (props) => {
 
     return (
         <form id="login_form" onSubmit={undefined}>
-            <input name="username" type="text" value={props.username} placeholder='Username' />
-            <input name="password" type="text" value={props.password} placeholder='Password' />
+            <input name="username" type="text" value={props.username} placeholder='Username' onChange={(e) => updateLoginForm({})}/>
+            <input name="password" type="text" value={props.password} placeholder='Password' onChange={(e) => updateLoginForm({name: e.target.name, value: e.target.value})}/>
             <input type="submit" value="Log In"></input>
         </form>
 
@@ -31,7 +31,7 @@ const Login = (props) => {
 // mapStateToProps is used for selecting the part of the data from the store that the connected component needs. 
 //mapState is what connects the data to the store -> take sttate from redux to get what the state is from redux
 function mapStateToProps(state) {
-    //Gives access to these parts of state as props
+    //Gives access to these parts of state as props --> Get the username and password from the store
   return { 
       username: state.loginForm.username,
       password: state.loginForm.password
@@ -42,4 +42,4 @@ function mapStateToProps(state) {
  //returns a component(the Login component)
  //mapstae is the first argument in connect
  //second argument is 
-  export default connect(mapStateToProps)(Login);
+  export default connect(mapStateToProps, {updateLoginForm: updateLoginForm})(Login);
