7ce5b0f0a3800ed99410d09bcbfbcce1eb55ba62
Hannah Malm <hannahemalm616@gmail.com>
2022-02-09 21:21:06 -0600

    added welcome component

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,3 +1,17 @@
 //Welcome page that is first loaded with a link to the login or register form
 //should be simple with just a picture
 //STATELESS - Will have LINKS
+
+import React from 'react';
+
+
+//render Welcome page with Header (Header has links to sign up or sign in)
+class Welcome extends React.Component {
+  render(){
+    return (
+   <div>Return Welcome Page Here with signup and sign in links</div>
+    );
+    }
+  }
+
+  export default Welcome;
