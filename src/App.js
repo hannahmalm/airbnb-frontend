e4e37c45a23bb78322d1d6bdf8ededf747cf7953
Hannah Malm <hannahemalm616@gmail.com>
2022-02-09 21:05:40 -0600

    updating footer and added into App

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import './App.css';
 //IMPORT COMPONENTS TO RENDER
 import Login from './components/LoginForm.js'
 import Header from './components/Header.js';
+import Footer from './components/Footer';
 
 
 //render Welcome page with Header (Header has links to sign up or sign in)
@@ -16,6 +17,7 @@ class App extends React.Component {
      <div>
        {/* <Login/> */}
        <Header></Header>
+       <Footer></Footer>
       </div>
     );
     }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,2 +1,18 @@
 //Nav bar with explore, reservations, and profile
 //stateless
+import React from 'react';
+
+
+//render Welcome page with Header (Header has links to sign up or sign in)
+class Footer extends React.Component {
+  render(){
+    return (
+     <div className='container'>
+         
+       
+      </div>
+    );
+    }
+  }
+
+  export default Footer;
