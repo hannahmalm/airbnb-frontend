b4967cd24a88397281baf8daaf48cd2b215fe640
Hannah Malm <hannahemalm616@gmail.com>
2022-02-10 12:44:32 -0600

    added in bootstrap cover code for welcome page

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -11,7 +11,31 @@ import React from 'react';
 class Welcome extends React.Component {
   render(){
     return (
-   <div>Return Welcome Page Here with signup and sign in links</div>
+   
+     <div className="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
+  <header className="mb-auto">
+    <div>
+      <h3 className="float-md-start mb-0">Cover</h3>
+      <nav className="nav nav-masthead justify-content-center float-md-end">
+        <a className="nav-link active" aria-current="page" href="#">Home</a>
+        <a className="nav-link" href="#">Features</a>
+        <a className="nav-link" href="#">Contact</a>
+      </nav>
+    </div>
+  </header>
+
+  <main className="px-3">
+    <h1>Cover your page.</h1>
+    <p className="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
+    <p className="lead">
+      <a href="#" className="btn btn-lg btn-secondary fw-bold border-white bg-white">Learn more</a>
+    </p>
+  </main>
+
+  <footer class="mt-auto text-white-50">
+    <p>Cover template for <a href="https://getbootstrap.com/" class="text-white">Bootstrap</a>, by <a href="https://twitter.com/mdo" class="text-white">@mdo</a>.</p>
+  </footer>
+</div>
     );
     }
   }
